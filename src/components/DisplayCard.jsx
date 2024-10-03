afc54c1e16132b093944dc9dd0ba268731bdbf3b
Tilak Rana <114658444+tilak-rana@users.noreply.github.com>
2024-10-03 22:39:52 +0530

    commit

--- src/components/DisplayCard.jsx
+++ src/components/DisplayCard.jsx
@@ -5,12 +5,14 @@ const DisplayCard = ({ onGroupingChange, onOrderingChange }) => {
         <div className="DisplayCard">
             <h5 className="groupTitle">Grouping</h5>
             <select name="group" id="groupId" onChange={(e) => onGroupingChange(e.target.value)}>
+                <option value="" disabled selected>select</option>
                 <option value="status">Status</option>
                 <option value="user">User</option>
                 <option value="priority">Priority</option>
             </select>
             <h5 className="orderTitle">Ordering</h5>
             <select name="order" id="orderId" onChange={(e) => onOrderingChange(e.target.value)}>
+                <option value="" disabled selected>select</option>
                 <option value="priority">Priority</option>
                 <option value="title">Title</option>
             </select>
